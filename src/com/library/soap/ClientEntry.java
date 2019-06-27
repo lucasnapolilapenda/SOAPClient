dbd9b69ee7b716362a01ce4a2e206da0a5ce3714
Lucas Napoli <lucasnapoli@wattopoststudio.com>
2019-06-27 00:27:04 -0400

    ****Deleting useless binary code
    ClientEntry Class to manage the Main Menu and app entry point
    Menu Pending
    
    ClientRequests to implement requests:
    Partially done
    listAllBooks
    getBookInfo
    getBookInfoByIsbn
    addBook
    UpdateBook
    DeleteBook
    
    All the flow has been reviews and data entry exception managed with a beautiful solution in the Menu
    Data Entry exceptions
    Review User data flow

--- src/com/library/soap/ClientEntry.java
+++ src/com/library/soap/ClientEntry.java
@@ -2,17 +2,74 @@ package com.library.soap;
 
 
 import java.net.MalformedURLException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 
 public class ClientEntry {
   public static void main(String[] argv) throws MalformedURLException {
 
-       new ClientRequests ().listAllbooks ();
-       new ClientRequests ().getBookInfo ();
-        new ClientRequests ().getBookInfoByIsbn ();
-        new ClientRequests ().addBook ();
+    showMenu ();
+
 
     }
 
+    public static void showMenu() throws MalformedURLException {
+        try {
+            int exit = 0;
+            do {
+                System.out.println ( "" );
+                System.out.println ( "****** Welcome to Solo's Library ******" );
+                System.out.println ( "" );
+                System.out.println ( "Please, Select one Option - Int Required from 0 to 6" );
+                System.out.println ( "1. Help / About" );
+                System.out.println ( "2. List Books" );
+                System.out.println ( "3. Display Book" );
+                System.out.println ( "4. Add Book" );
+                System.out.println ( "5. Update Book" );
+                System.out.println ( "6. Delete Book" );
+                System.out.println ( "0. Quit" );
+                System.out.println ( "" );
+
+                Scanner sc = new Scanner ( System.in );
+                int response = Integer.valueOf ( sc.nextLine ( ) );
+                System.out.println ( response );
+                switch (response) {
+                    case 0:
+                        //salir
+                        exit = 0;
+                        break;
+                    case 1:
+                        new ClientRequests ().help ();
+                        break;
+                    case 2:
+                        new ClientRequests ().listAllbooks ();
+                        break;
+                    case 3:
+                        new ClientRequests ().displaySelector ();
+                        break;
+                    case 4:
+                        new ClientRequests ().addBook ();
+                        break;
+                    case 5:
+                        new ClientRequests ().updateBook ();
+                        break;
+                    case 6:
+                        new ClientRequests ().deleteBook ();
+                        break;
+
+                    default:
+                        System.out.println ( );
+                        System.out.println ( "Please, Select one Option" );
+                        System.out.println ( );
+                        break;
+                }
+
+            } while (exit != 0);
+        } catch (InputMismatchException | NumberFormatException ex) {
+            System.out.println ( "Please, check the input, all (int) required values" );
+            showMenu ();
+    }
   }
-    // Please, do not remove this line from file template, here invocation of web service will be inserted
+
+}
--- src/com/library/soap/ClientRequests.java
+++ src/com/library/soap/ClientRequests.java
@@ -5,58 +5,70 @@ import javax.xml.ws.Service;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
-import java.util.function.Consumer;
 
 
 public class ClientRequests {
-
-    public LibraryServices libraryServicesCreation () throws MalformedURLException {
-        URL wsdlURL = new URL("http://localhost:8080/soap_war/library?wsdl");
-        QName qname = new QName("http://soap.lucas.com/", "LibraryServicesService");
-        Service service = Service.create(wsdlURL, qname);
-        LibraryServices libraryServices = service.getPort(LibraryServices.class);
-
-        return libraryServices;
-    }
 
     public void listAllbooks () throws MalformedURLException {
         LibraryServices rq = libraryServicesCreation ();
 
         System.out.println ( ":::::: Module List of Books ::::::" );
         System.out.println (rq.listAllBooks () );
-
+        ClientEntry.showMenu ( );
     }
 
     public void getBookInfo () throws MalformedURLException {
         LibraryServices rq = libraryServicesCreation ();
-        Scanner sc = new Scanner ( System.in );
 
-        System.out.println ( ":::::: Module Id search ::::::" );
-        System.out.println ( "Please enter the id" );
-        int response = Integer.valueOf ( sc.nextLine () );
+        int response = idRequest ( false );
         System.out.println ( rq.getBookInfo ( response ) );
-
+        ClientEntry.showMenu ( );
     }
 
     public void getBookInfoByIsbn () throws MalformedURLException {
         LibraryServices rq = libraryServicesCreation ();
         Scanner sc = new Scanner ( System.in );
 
-
         System.out.println ( ":::::: Module ISBN search ::::::" );
         System.out.println ( "Please enter the ISBN" );
         String response = sc.nextLine ( );
         System.out.println ( rq.getBookInfoByISBN ( response ) );
-
+        ClientEntry.showMenu ( );
     }
 
     public void addBook () throws MalformedURLException {
         LibraryServices rq = libraryServicesCreation ();
+        Book book = preparationBook ( false );
+
+        System.out.println ( rq.addBook ( book ) );
+        ClientEntry.showMenu ( );
+    }
+
+    public void updateBook () throws MalformedURLException {
+        LibraryServices rq = libraryServicesCreation ();
+        Book book = preparationBook ( true );
+
+        System.out.println ( rq.updateBookInfo ( book ) );
+        ClientEntry.showMenu ( );
+    }
+
+    public void deleteBook () throws MalformedURLException {
+        LibraryServices rq = libraryServicesCreation ();
+
+        int response = idRequest ( true );
+        System.out.println ( rq.deleteBook ( response ) );
+        ClientEntry.showMenu ( );
+    }
+
+    public Book preparationBook (boolean update) {
+
         Scanner sc = new Scanner ( System.in );
         Book book = new Book ();
 
-        System.out.println ( ":::::: Module Add new Book ::::::" );
-        System.out.println ( "Please enter the Id" );
+        if (update) { System.out.println ( ":::::: Module Update new Book ::::::" ); }
+        else { System.out.println ( ":::::: Module Add new Book ::::::" ); }
+
+        System.out.println ( "Please enter the Id - Int Required" );
         book.setId (Integer.valueOf (sc.nextLine ()  ) );
         System.out.println ( "Please enter the Isbn" );
         book.setIsbn ( sc.nextLine () );
@@ -69,7 +81,59 @@ public class ClientRequests {
         System.out.println ( "Please enter the Description" );
         book.setDescription ( sc.nextLine () );
 
-        System.out.println ( rq.addBook ( book ) );
+        return book;
+    }
+
+    public void displaySelector () throws MalformedURLException {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ( "Press. 1 + Enter to search by Id or Press. 2 + Enter to search by ISBN - Int Required from 1 to 2 " );
+        int option = Integer.valueOf ( sc.nextLine () );
+
+
+        while (!(option == 1 ||  option ==2)) {
+            System.out.println ( "Press. 1 + Enter to search by Id or Press. 2 + Enter to search by ISBN - Int Required from 1 to 2" );
+            option = Integer.valueOf ( sc.nextLine () );
+        }
+        if (option == 1) {
+            getBookInfo ();
+        }
+
+        else {
+            getBookInfoByIsbn ();
+        }
+
+    }
+
+    public void help() throws MalformedURLException {
+        System.out.println ( "***** Welcome to Solo's Library System *****" );
+        System.out.println (    "\n In this System you can search, add and delete books. " +
+                "\n Additionally, you can work remotely with web services, you " +
+                "\n should be sure that TomCat is running and be sure all dependencies installed" +
+                "\n Thanks for using Lucas Software and enjoy it" );
+
+        System.out.println ( "Please, type any KEY to come back to the Menu" );
+        Scanner sc = new Scanner ( System.in );
+        sc.next ( );
+        ClientEntry.showMenu ( );
+    }
+
+    public int idRequest (boolean delete) {
+        Scanner sc = new Scanner ( System.in );
+        if (delete) {System.out.println ( "::::::  Module Id Delete ::::::" ); }
+        else {System.out.println ( ":::::: Module Id Search ::::::" );}
+
+        System.out.println ( "Please enter the id - Int Required " );
+        int response = Integer.valueOf ( sc.nextLine () );
+        return response;
+    }
+
+    public LibraryServices libraryServicesCreation () throws MalformedURLException {
+        URL wsdlURL = new URL("http://localhost:8080/soap_war/library?wsdl");
+        QName qname = new QName("http://soap.lucas.com/", "LibraryServicesService");
+        Service service = Service.create(wsdlURL, qname);
+        LibraryServices libraryServices = service.getPort(LibraryServices.class);
+
+        return libraryServices;
     }
 
 }
